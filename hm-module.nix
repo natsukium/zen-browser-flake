71f09015db515eef104b59c2f3656d14dff3ebbb
natsukium <tomoya.otabi@gmail.com>
2025-04-18 08:06:09 +0900

    module: make programs.zen-browser.package overridable
    This commit makes the Zen Browser package overridable using
    lib.mkDefault.
    Users can now replace the default package with their own, for example,
    enabling the same module to be used on Darwin platforms by providing a
    compatible package.

--- hm-module.nix
+++ hm-module.nix
@@ -37,7 +37,7 @@ in {
 
   config = lib.mkIf config.programs.zen-browser.enable {
     programs.zen-browser = {
-      package = self.packages.${pkgs.stdenv.system}.${name};
+      package = lib.mkDefault self.packages.${pkgs.stdenv.system}.${name};
       policies = {
         DisableAppUpdate = lib.mkDefault true;
         DisableTelemetry = lib.mkDefault true;
